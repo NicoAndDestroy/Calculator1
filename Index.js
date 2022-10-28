a1e04553291121d6bd3d9eabbaacd373555ffac2
NicoAndDestroy <>
2022-10-28 04:59:13 +0000

    Initial commit

---
+++ Index.js
@@ -0,0 +1,31 @@
+//Suma. resta, multiplicacion, division
+
+function suma () {
+  resultado = 5 + 6
+  console.log(resultado)
+};
+
+suma()
+
+function resta () {
+  resultado = 5 - 6
+  console.log(resultado)
+};
+
+resta()
+
+function division () {
+  resultado = 5 / 6
+  console.log(resultado)
+};
+
+division()
+
+function multiplicacion () {
+  resultado = 5 * 6
+  console.log(resultado)
+};
+
+multiplicacion()
+
+
